874be3bc6de41faf12af14dfe91e4a419285a9d7
Gustavo Figueiredo <68259742+GustaFTW@users.noreply.github.com>
2023-03-03 12:31:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from PIL import Image
 
-hamburguer = Image.open('C:\Users\Gusta\Documents\CodeStuff\Machine_Learning\Git Uploads\Food101-streamlit\pics\230446.png')
-ice_cream = Image.open('C:\Users\Gusta\Documents\CodeStuff\Machine_Learning\Git Uploads\Food101-streamlit\pics\321321.png')
-pizza = Image.open('C:\Users\Gusta\Documents\CodeStuff\Machine_Learning\Git Uploads\Food101-streamlit\pics\pizza.png')
-base_model_structure = Image.open("C:\Users\Gusta\Documents\CodeStuff\Machine_Learning\Git Uploads\Food101-streamlit\pics\base_model_structure.png")
-model_structure = Image.open("C:\Users\Gusta\Documents\CodeStuff\Machine_Learning\Git Uploads\Food101-streamlit\pics\model_structure.png")
+hamburguer = Image.open('C:\\Users\\Gusta\\Documents\\CodeStuff\\Machine_Learning\\Git Uploads\\Food101-streamlit\\pics\\230446.png')
+ice_cream = Image.open('C:\\Users\\Gusta\\Documents\\CodeStuff\\Machine_Learning\\Git Uploads\\Food101-streamlit\\pics\\321321.png')
+pizza = Image.open('C:\\Users\\Gusta\\Documents\\CodeStuff\\Machine_Learning\\Git Uploads\\Food101-streamlit\\pics\\pizza.png')
+base_model_structure = Image.open("C:\\Users\\Gusta\\Documents\\CodeStuff\\Machine_Learning\\Git Uploads\\Food101-streamlit\\pics\\base_model_structure.png")
+model_structure = Image.open("C:\\Users\\Gusta\\Documents\\CodeStuff\\Machine_Learning\\Git Uploads\\Food101-streamlit\\pics\\model_structure.png")
 
 
 st.title("Food Recognition")
